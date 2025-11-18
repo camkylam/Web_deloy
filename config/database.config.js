5de1703fc04b4eecf9554047d5ea929eb26b0632
unknown <camkylam@gmail.com>
2025-11-18 11:07:36 +0700

    Fix: Encode password in MongoDB connection string

--- config/database.config.js
+++ config/database.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://lcky:lamcamky123cluster0.skjwh.mongodb.net/?appName=Cluster0');
+        await mongoose.connect('mongodb+srv://lcky:lamcamky123@cluster0.skjwh.mongodb.net/?appName=Cluster0');
         console.log('Kết nối MongoDB thành công');
     } catch (err) {
         console.error('Lỗi kết nối MongoDB:', err);
